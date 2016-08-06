211caf3dd8bed5cba4819485e09d4308977bfdcc
Sergey <krooshch@gmail.com>
2016-08-06 15:42:29 +1000

    Changed folder structure to add unit tests

--- Dotson/Reading/Token.cs
+++ Dotson/Reading/Token.cs
@@ -32,11 +32,14 @@
         {
         }
 
-        public override string ToString()
+        public override bool Equals(object obj)
         {
-            return base.ToString();
+            return base.Equals(obj);
         }
 
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
-
 }
